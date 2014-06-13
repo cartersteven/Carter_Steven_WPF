6b9910048b4413b3a73c2ea3b491c5bb9bd25d5b
cartersteven <stevencarter@fullsail.edu>
2014-06-13 13:59:24 -0400

    Added Associated_JS files.
    Added Associated_JS files.

---
+++ Associating_js/defining_variables.js
@@ -0,0 +1,10 @@
+//Defining Variables
+/**
+ * Created by shaunacarter on 6/4/14.
+ */
+var age; //declaration
+age = 19; //definition
+
+//storage container = value
+
+var yearOfbirth = 1994; //declaring and defining on the same line
