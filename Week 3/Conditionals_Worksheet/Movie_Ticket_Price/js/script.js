1c20f8d74aa4a2de1bb7811a7c6c18b8f551335a
cartersteven <stevencarter@fullsail.edu>
2014-06-17 13:48:50 -0400

    Movie Ticket Price
    This finishes the Movie Ticket Price Calculator.

--- Week 3/Conditionals_Worksheet/Movie_Ticket_Price/js/script.js
+++ Week 3/Conditionals_Worksheet/Movie_Ticket_Price/js/script.js
@@ -9,4 +9,19 @@ var time = prompt("What is the time?"); //This ascertains the time of the movie.
 if (age < 2){
     //This states that small children should not be taken to movie theaters.
     console.log("Small children should not be taken into a movie theater. They can cause distractions.")
-} else if ()
+} else if (age < 10){
+    //This states that for children, the ticket price is $7.
+    console.log("The ticket price is $" + 7);
+} else if (age > 55){
+    //this states that for seniors, the ticket price is $7.
+    console.log("The ticket price is $" + 7);
+} else if (time < 3){
+    //this states that before 3, the ticket price is $12.
+    console.log("The ticket price is $" + 12);
+} else if (time >5){
+    //this states that after 5, the ticket price is $12.
+    console.log("The ticket price is $" + 12);
+} else {
+    //this states that for all other conditions, the ticket price is $7.
+    console.log("The ticket price is $" + 7);
+}
