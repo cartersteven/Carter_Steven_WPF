472001271d3b271be2418a4a212b3d0693a2e1c7
cartersteven <stevencarter@fullsail.edu>
2014-06-17 13:41:13 -0400

    Movie Ticket Price
    This commit starts the movie ticket price calculator. And it sets up
    the starting variables and the beginning of the conditional.

---
+++ Week 3/Conditionals_Worksheet/Movie_Ticket_Price/js/script.js
@@ -0,0 +1,12 @@
+//This program will calculate the movie price based on time and age.
+/**
+ * Created by stevencarter on 6/17/14.
+ */
+
+var age = prompt("What age are you?"); //This prompt ascertains the viewer's age.
+var time = prompt("What is the time?"); //This ascertains the time of the movie.
+
+if (age < 2){
+    //This states that small children should not be taken to movie theaters.
+    console.log("Small children should not be taken into a movie theater. They can cause distractions.")
+} else if ()
