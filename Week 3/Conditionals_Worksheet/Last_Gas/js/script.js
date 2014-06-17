c4ab8c399ce24ebb645af917b84941b44cadce8c
cartersteven <stevencarter@fullsail.edu>
2014-06-17 13:31:05 -0400

    Edited changes to Last_Gas
    This added the comments labeling the code to the code.

--- Week 3/Conditionals_Worksheet/Last_Gas/js/script.js
+++ Week 3/Conditionals_Worksheet/Last_Gas/js/script.js
@@ -3,18 +3,20 @@
  * Created by stevencarter on 6/16/14.
  */
 
-var percent = prompt("What is the percent reading on the fuel gauge of your car?");
-var capacity = prompt("What is the maximum capacity of your fuel tank in gallons?");
-var mileage = prompt("What is the average fuel mileage attained by your car?");
+var percent = prompt("What is the percent reading on the fuel gauge of your car?"); //This will ask the user what the fuel reading on their dashboard is.
+var capacity = prompt("What is the maximum capacity of your fuel tank in gallons?"); //This will ask the user what the maximum fuel capacity of their car is.
+var mileage = prompt("What is the average fuel mileage attained by your car?"); //This will ask the user what the average fuel mileage of their car is.
 
-var distance = 200;
-var percentMul = percent / 100;
+var distance = 200; //This sets the distance to the next fuel station to 200 miles.
+var percentMul = percent / 100; //This makes the percent remaining in the tank into a number instead of a percentage.
 
-var gallons = percentMul * capacity
-var range = (percentMul * capacity) * mileage;
+var gallons = percentMul * capacity; //This finds out how many gallons of fuel are left in the tank.
+var range = (percentMul * capacity) * mileage; //This calculates the remaining range of the car.
 
 if (range > distance){
+    //This prints the results of the above calculation to the console in the event that there is enough fuel left.
     console.log("Yes, you can make it without stopping for gas!");
 } else {
+    //This prints the results of the above calculations to the console in the event that there is enough fuel left.
     console.log("You only have " + gallons + " gallons of gas in your tank, better get gas now while you can!");
 }
