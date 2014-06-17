9aa5c62f73e39a2eab9b34889981bbfee4307547
cartersteven <stevencarter@fullsail.edu>
2014-06-16 22:19:01 -0400

    Relational Expressions
    This is the work from the Relational Expressions screencast.

---
+++ Relational Expressions/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by shaunacarter on 6/16/14.
+ */
+var kidHeight = 30;
+var minHeight = 48;
+
+if (kidHeight > minHeight){
+    console.log("You can ride the coaster!");
+} else {
+    console.log("You cannot ride the ride.";)
+}
