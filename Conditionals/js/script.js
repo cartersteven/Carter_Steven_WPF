447fbd0d0b81b11a5057f5702c34a8a174338e78
cartersteven <stevencarter@fullsail.edu>
2014-06-16 22:14:22 -0400

    Conditional Logic
    This is the work from the screencast: Conditional Logic.

---
+++ Conditionals/js/script.js
@@ -0,0 +1,13 @@
+//Conditional Logic
+/**
+ * Created by stevencarter on 6/16/14.
+ */
+
+var oldEnough = true;
+
+//If the child is old enough, print to the console, "You can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("What comes after");
