9abe77ef725a2bf445efd8b2b568c8bd78f26c79
cartersteven <stevencarter@fullsail.edu>
2014-06-17 14:56:49 -0400

    Average Speed calculator
    This calculator will calculate if you will make it to your destination
    that is a variable miles away, with a variable time left, and a
    variable current average speed.

--- Week 3/Conditionals_Graded/Conditionals_Personal/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Personal/js/script.js
@@ -1,4 +1,19 @@
-//This program will
+//This program will calculate if a traveler will make it to their destination given that they are traveling at an average speed. if they are not traveling at the needed minimum, the console will print the needed minimum.
 /**
  * Created by stevencarter on 6/17/14.
  */
+
+var avg = prompt("What is your current average speed, in miles per hour?"); //This prompt asks the user how fast they are going on average.
+var dist = prompt("What is the distance, in miles, that needs to be covered?"); //This prompt asks the user how far they are from their destination.
+var time = prompt("What is the remaining time needed to reach the destination, in minutes?"); //This prompt asks the user how many minutes they have left.
+
+var timeH = time / 60; //This turns the time variable into hours.
+var speedNeed = dist / timeH; //This calculates the needed speed to make it to the destination on time.
+
+if (speedNeed <= avg){
+    //This will print if the speed needed is less than the speed currently made.
+    console.log("You will make it to the destination on time")
+} else {
+    //This will print if the speed needed is greater than the current speed.
+    console.log("You will not make it on time. The average speed that you need to maintain is at least " + speedNeed + "mph.")
+}
