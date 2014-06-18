e07477a7219f9538790e9b90e78023f0a3440c01
cartersteven <stevencarter@fullsail.edu>
2014-06-18 16:22:16 -0400

    Conditionals Industry
    This commit adds the prompts for the Industry program. Variables not
    used yet.

--- Week 3/Conditionals_Graded/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Industry/js/script.js
@@ -1,4 +1,10 @@
-//This program will.
+//This program will determine if an image will fit into a space in a div after a 10px order is added.
 /**
  * Created by Stevencarter on 6/18/14.
  */
+
+var imHeight = prompt("What is the height of the image?");
+var imWidth = prompt("What is the width of the image?");
+var holeHeight = prompt("What is the height of the page div?");
+var holewidth = prompt("What is the width of the page div?");
+
