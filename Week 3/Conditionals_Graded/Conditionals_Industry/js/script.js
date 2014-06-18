d3f4222f2967a956e4b0b3da195ffdde347c3d67
cartersteven <stevencarter@fullsail.edu>
2014-06-18 16:46:01 -0400

    Conditionals Industry
    Added the beginning of the conditional statement to the program. This
    validates the inputs and makes sure that there is an input actually
    there.

--- Week 3/Conditionals_Graded/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Industry/js/script.js
@@ -1,12 +1,33 @@
-//This program will determine if an image will fit into a space in a div after a 10px order is added.
+//This program will determine if an image will fit into a space in a div after a border is added.
 /**
  * Created by Stevencarter on 6/18/14.
  */
 
-var imHeight = prompt("What is the height of the image?"); //This asks the user how high the image they want to use is.
-var imWidth = prompt("What is the width of the image?"); //This asks the user how wide the image is.
-var holeHeight = prompt("What is the height of the page div?"); //This asks the user how high the div in the page is.
-var holewidth = prompt("What is the width of the page div?"); //This asks the user how wide the div is.
+var imHeight = prompt("What is the height of the image, in pixels?"); //This asks the user how high the image they want to use is.
+var imWidth = prompt("What is the width of the image, in pixels?"); //This asks the user how wide the image is.
+var holeHeight = prompt("What is the height of the page div, in pixels?"); //This asks the user how high the div in the page is.
+var holeWidth = prompt("What is the width of the page div, in pixels?"); //This asks the user how wide the div is.
+var borSize = prompt("What is the value of the size of the border, in pixels?"); //Ths asks the user what the size of the border they are adding is.
 
-var borHeight = imHeight + 20; //This adds the total size of the border to the height of the image
-var borWidth = imWidth + 20; //This adds the total size of the border to the width of the image
+var borHeight = imHeight + (2 * borSize); //This adds the total size of the border to the height of the image
+var borWidth = imWidth + (2 * borSize); //This adds the total size of the border to the width of the image
+
+if (imHeight = ""){
+    //This makes sure there is a value entered into imHeight.
+    console.log("You left the image height prompt empty. Please start again.")
+} else if (imWidth = ""){
+    //This makes sure there is a value entered into imWidth.
+    console.log("You left the image width prompt empty. Please start again.")
+} else if (holeHeight = ""){
+    //This makes sure there is a value entered into holeHeight.
+    console.log("You left the div height prompt empty. Please start again.")
+} else if (holeWidth = ""){
+    //This makes sure there is a value entered into holeWidth.
+    console.log("You left the div width prompt empty. Please start again.")
+} else if (borSize = ""){
+    //This makes sure there is a value entered into borSize.
+    console.log("You left the Border Size prompt empty. Please start again.")
+} else if (borSize < 0){
+    //This makes sure there is a positive or zero value.
+    console.log("Please start again. There can not be a negative border value. The value that you entered is " + borSize + ".")
+} else
