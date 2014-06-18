f38873690d65f558cd641fd47d945b53c6dc500d
cartersteven <stevencarter@fullsail.edu>
2014-06-18 16:26:40 -0400

    Conditionals Industry
    Added expression to calculate the new width and height of the image
    based on a set border size of 10. May redesign to make the border a
    changeable variable.

--- Week 3/Conditionals_Graded/Conditionals_Industry/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Industry/js/script.js
@@ -3,8 +3,10 @@
  * Created by Stevencarter on 6/18/14.
  */
 
-var imHeight = prompt("What is the height of the image?");
-var imWidth = prompt("What is the width of the image?");
-var holeHeight = prompt("What is the height of the page div?");
-var holewidth = prompt("What is the width of the page div?");
+var imHeight = prompt("What is the height of the image?"); //This asks the user how high the image they want to use is.
+var imWidth = prompt("What is the width of the image?"); //This asks the user how wide the image is.
+var holeHeight = prompt("What is the height of the page div?"); //This asks the user how high the div in the page is.
+var holewidth = prompt("What is the width of the page div?"); //This asks the user how wide the div is.
 
+var borHeight = imHeight + 20; //This adds the total size of the border to the height of the image
+var borWidth = imWidth + 20; //This adds the total size of the border to the width of the image
