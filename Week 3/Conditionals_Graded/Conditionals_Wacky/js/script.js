d50b2576dc8fb445cc5b8476ad0baf130464c338
cartersteven <stevencarter@fullsail.edu>
2014-06-19 11:50:57 -0400

    Conditionals Wacky
    This commit adds the rest of the prompts and the beginning of the error
    checking.

--- Week 3/Conditionals_Graded/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Wacky/js/script.js
@@ -3,10 +3,23 @@
  * Created by stevencarter on 6/17/14.
  */
 
-var weight = [0,1,2,3,4,130,0.375,7,8,9,10,2568,0.123,143]; //This Array holds the weights of the items to be inserted
+var weightList = [0,1,2,3,4,130,0.375,7,8,9,10,2568,0.123,143]; //This Array holds the weights of the items to be inserted
                                                             //into the elevator.The reason that the actual values are
                                                             //spread out is because the input into the prompt asking
                                                             // which one will find how many letters there are and use
                                                             // that space in the array
 
-var cap = prompt("What is the capacity of the elevator?");
+var cap = prompt("What is the capacity of the elevator?"); //This asks the user what the capacity of the elevator is.
+var item = prompt("Which item do you want to put into the elevator? Will it be . . .\n'Banana'?\n'Sheep'?\n'Mini Cooper'?\n'Rubber Ducky'?\n'Mister Rogers'?"); //This asks the user what item they want to put in the elevator.
+
+var weight = weightList[item.length]; //This will take the item entered and convert it into weight.
+if (cap == ""){
+    //This will display an error if there is nothing entered into the first prompt.
+    console.log("You did not enter anything into the first prompt. Please Refresh and try again.")
+} else if (item == ""){
+    //This will display an error if there is nothing entered into the second prompt.
+    console.log("You did not enter anything into the second prompt. Please Refresh and try again.")
+} else if (weight > cap){
+    //This will produce an error that states that not a single item can fit.
+    
+}
