11d49dde830a123c803bfce50d6b94f59e2cfc87
cartersteven <stevencarter@fullsail.edu>
2014-06-19 12:04:10 -0400

    Conditionals Wacky
    Fixed misspelling math error. Made numberFit calculate to zero decimals.

--- Week 3/Conditionals_Graded/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Wacky/js/script.js
@@ -3,7 +3,7 @@
  * Created by stevencarter on 6/17/14.
  */
 
-var weightList = [0,1,2,3,4,130,0.375,7,8,9,10,2568,0.123,143]; //This Array holds the weights of the items to be inserted
+var weightList = [0,0,0,0,0,130,0.375,0,0,0,0,2568,0.123,143]; //This Array holds the weights of the items to be inserted
                                                             //into the elevator.The reason that the actual values are
                                                             //spread out is because the input into the prompt asking
                                                             // which one will find how many letters there are and use
@@ -14,6 +14,7 @@ var item = prompt("Which item do you want to put into the elevator? Will it be .
 var numberFit = 0; //This variable sets up the number that can fit into an elevator.
 
 var weight = weightList[item.length]; //This will take the item entered and convert it into weight.
+
 if (cap == ""){
     //This will display an error if there is nothing entered into the first prompt.
     console.log("You did not enter anything into the first prompt. Please Refresh and try again.");
@@ -23,8 +24,11 @@ if (cap == ""){
 } else if (weight > cap){
     //This will produce an error that states that not a single item can fit.
     console.log("You cannot fit a single " + item + " onto this elevator.");
+} else if (weight ==0){
+    //this will print an error if the item is mispelled
+    console.log("You typed something outside of the program parameters. Please try again.")
 } else {
     //This divides the capacity by the item's weight and displays how many can fit onto the elevator.
-    numberFit = cap / weight;
-    console.log("You can fit " + numberFit + item + "'s onto this elevator.")
+    numberFit = cap / weight | 0;
+    console.log("You can fit " + numberFit + " " + item + "'s onto this elevator.")
 }
