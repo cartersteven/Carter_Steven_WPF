39b4d5396a75aca699ee9872cf5147c294c2df9c
cartersteven <stevencarter@fullsail.edu>
2014-06-19 11:33:50 -0400

    Conditionals Wacky
    Added Data Array and first prompt.

--- Week 3/Conditionals_Graded/Conditionals_Wacky/js/script.js
+++ Week 3/Conditionals_Graded/Conditionals_Wacky/js/script.js
@@ -3,3 +3,10 @@
  * Created by stevencarter on 6/17/14.
  */
 
+var weight = [0,1,2,3,4,130,0.375,7,8,9,10,2568,0.123,143]; //This Array holds the weights of the items to be inserted
+                                                            //into the elevator.The reason that the actual values are
+                                                            //spread out is because the input into the prompt asking
+                                                            // which one will find how many letters there are and use
+                                                            // that space in the array
+
+var cap = prompt("What is the capacity of the elevator?");
