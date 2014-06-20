28f7521217e289ee8f1a57cb3e1078c269399101
cartersteven <stevencarter@fullsail.edu>
2014-06-20 14:26:35 -0400

    Arguments and Parameters
    This commit contains the work for the Arguments and Parameters
    screencast.

--- Week 4/Arguements_and_Parameters/js/script.js
+++ Week 4/Arguements_and_Parameters/js/script.js
@@ -1,3 +1,12 @@
 /**
  * Created by shaunacarter on 6/20/14.
  */
+function dogYears(age){
+    var dogYears = age * 7;
+    console.log("Sparky is " + dogYears + " years old.");
+}
+
+var age1 = 4;
+dogYears(age1);
+dogYears(prompt("How old in calendar years is sparky?"));
+dogYears(5);
