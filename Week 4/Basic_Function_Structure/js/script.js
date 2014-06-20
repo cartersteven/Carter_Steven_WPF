feddc8d2e3d513b81e0d53885228fcaef92494f6
cartersteven <stevencarter@fullsail.edu>
2014-06-20 13:40:47 -0400

    Basic Function Structure
    This commit contains the work for the basic function structure
    screencast.

---
+++ Week 4/Basic_Function_Structure/js/script.js
@@ -0,0 +1,15 @@
+//Basic Function Structure
+/**
+ * Created by Stevencarter on 6/20/14.
+ */
+
+function outptMsg(){
+    console.log("Hello World!");
+}
+
+function calcArea (){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
