624d50c1c0066183a05b33d8cc576a6c892a1ba4
cartersteven <stevencarter@fullsail.edu>
2014-06-21 16:10:40 -0400

    Personal Functions
    Started work. First test in progress.

--- Week 4/Functions_Graded/Functions_Personal/js/script.js
+++ Week 4/Functions_Graded/Functions_Personal/js/script.js
@@ -1,4 +1,20 @@
-//This program will .
+//This program will calculate the caffeine taken in on a day and display how much.
+//If it is over the FDA guidelines, the program will display an error code.
 /**
  * Created by stevencarter on 6/21/14.
  */
+var test = function(){
+    console.log("The test worked!");
+};
+var drinks = prompt("How many drinks have you had today?\nBe honest.");
+if (drinks == ""){
+    alert("Please start again. You did not enter a value.");
+    console.log("Please start again. You did not enter a value.");
+} else if (drinks <= 0){
+    alert("Please start again. The value cannot be at or below zero.");
+    console.log("Please start again. The value cannot be at or below zero");
+} else {
+    while(drinks > 0){
+        prompt("What was the next drink based on?");
+    }
+}
