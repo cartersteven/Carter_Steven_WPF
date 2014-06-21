ba35535ad39cb34127f32ffebab58ef7910a1161
cartersteven <stevencarter@fullsail.edu>
2014-06-21 14:43:45 -0400

    Loops
    The work for videos 2 and 3 of Loops.

--- Week 4/Loops/js/script.js
+++ Week 4/Loops/js/script.js
@@ -2,10 +2,26 @@
 /**
  * Created by shaunacarter on 6/21/14.
  */
-
+/*console.log("----------- While Loops -------------");
 var b = 10;
 
 while (b > 0){
     console.log(b + " kegs on the wall.");
     b--;
+}
+*/
+/*console.log("----------- Do While Loops -------------");
+
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while {c > 10};
+    */
+console.log("------------ For Loops ------------");
+
+for(var i = 10 ; i > 0; i--){
+    console.log(i + " kegs on the wall.");
 }
