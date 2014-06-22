08bd648de23436b8cea3d3cf38afe2f3fc2a8855
cartersteven <stevencarter@fullsail.edu>
2014-06-21 23:15:06 -0400

    Functions Personal
    This commit finishes the functions and the summation line, then starts
    the final output sequence.

--- Week 4/Functions_Graded/Functions_Personal/js/script.js
+++ Week 4/Functions_Graded/Functions_Personal/js/script.js
@@ -11,11 +11,29 @@ var coffee = function(){
     //grande = 6 = 125mg
     //venti = 5 = 170mg
     var milligrams = [0,0,0,60,90,170,125];
-    var print = milligrams[prompt("type 'mug', stupid.").length];
+    var print = milligrams[prompt("Which drip-coffee based drink are you using here? Your options are:\nA 'Mug'\nA 'Tall' Starbucks Iced Coffee with Milk\nA 'Grande' Starbucks Iced Coffee with Milk\n Or a 'Venti' Starbucks Iced Coffee with Milk").length];
     return print;
 }; //This function will gather data from the user about which drink they drank.
+var espresso = function(){
+    //This section of comments makes note of the size and strength of several drinks that I commonly drink using espresso.
+    //"Weak" Iced Espresso at home = 4 = 180mg
+    //"Strong" Iced Espresso at home = 6 = 360mg
+    //"Venti" Iced Caramel Macchiato = 5 = 225mg
+    var milligrams = [0,0,0,0,180,225,360];
+    var print = milligrams[prompt("Which espresso based drink are you using here? Your options are:\nA 'Weak' Iced Espresso at home\nA 'Strong' Iced Espresso at home\n Or a 'Venti' Starbucks Iced Caramel Macchiato").length];
+    return print;
+};
+var energy = function(){
+    //This section of comments makes note of the size and strength of several energy drinks that I commonly enjoy.
+    //"Little NOS" = 10 = 160mg
+    //"Big NOS" = 7 = 220mg
+    //Any "Monsters" = 8 = 180mg
+    var milligrams = [0,0,0,0,0,0,0,220,180,0,160];
+    var print = milligrams[prompt("Which energy drink are you using here? Your options are:\nA 'Little NOS'\nA 'Big NOS'\nOr any of the various 'Monsters'").length];
+    return print;
+};
 
-var coffeeTotal = [] , espressoTotal = [] , energyTotal = [];
+var caffeineTotal = [];
 
 var drinks = prompt("How many drinks have you had today?\nBe honest.");
 if (drinks == ""){
@@ -27,28 +45,23 @@ if (drinks == ""){
 } else {
 
     while(drinks > 0){
-        var callFunc = prompt("What was the next drink based on?\nEnter the number zero if you made an error and entered a higher number in the number of drinks question than intended.");
-        if (callFunc == "coffee"){
-            coffeeTotal.push(coffee())
-        } else if(callFunc == "espresso"){
-            espressoTotal.pust(espresso())
-        } else if (callFunc == "energy"){
-            energyTotal.push(energy())
+        var callFunc = prompt("What was the next drink based on?\nCoffee, Espresso, or Energy.");
+        if (callFunc == "coffee" || callFunc == "Coffee"){
+            caffeineTotal.push(coffee())
+        } else if(callFunc == "espresso" || callFunc == "Espresso"){
+            caffeineTotal.push(espresso())
+        } else if (callFunc == "energy" || callFunc == "Energy"){
+            caffeineTotal.push(energy())
         } else if (callFunc == 0){
-
         } else {
             console.log('"' + callFunc + '" is not one of the accepted responses to the question.');
         }
         drinks--;
     }
 }
+var totalTotal = eval(caffeineTotal.join('+'));
 
-console.log(coffeeTotal , espressoTotal , energyTotal)
-
-/*var repeat, studentArr = [], markArr = [];
-while (repeat !== 'n' && repeat !== 'N'){
-    studentArr.push(prompt("Enter Student Name: "));
-    markArr.push(prompt("Enter Student Mark: "));
-    repeat = prompt ("Do you want to enter another student: y/n");
+if(totalTotal >= 400){
+    
 }
-console.log('studentArr, markArr',studentArr, markArr);*/
+
