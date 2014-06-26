83eebd6cbea65d7db3026fe685d597575a0a3670
cartersteven <stevencarter@fullsail.edu>
2014-06-26 19:50:15 -0400

    Functions Wacky
    This commit is badly spaced from the last one and has the program
    finished and tested and waiting for comments to finalize it.

--- Week 4/Functions_Graded/Functions_Wacky/js/script.js
+++ Week 4/Functions_Graded/Functions_Wacky/js/script.js
@@ -4,17 +4,38 @@
  */
 
 var randomnumber=Math.floor(Math.random()*101);
-
+var guessbank = [];
+var diffbank = [];
 var userinput = function(number){
-    var result = randomnumber - number;
+    result = Math.abs(randomnumber - number);
     return result;
 };
+
 
 alert("This is a game where players try to guess what number the computer is thinking on a scale of 0 to 100.");
 var players = prompt("How many players are playing?");
 
 while (players < 0) {
     players = prompt("ERROR\n\nYou have input an invalid value. Please try again.");
+}
+var playernumber = 1;
+
+while (players > 0) {
+    var guess = prompt("What is player " + playernumber + "'s guess?");
+    guessbank.push(guess);
+    diffbank.push(userinput(guess));
+    playernumber++;
+    players--;
+}
+
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
 };
 
-console.log(players);
+var minimum = Array.min(diffbank);
+
+var pos = diffbank.indexOf(minimum);
+
+var winPos = pos + 1;
+
+console.log("Player " + winPos + " is the winner! They guessed " + guessbank[pos] + " which was only " + minimum + " away from the computer's number of " + randomnumber + ".");
