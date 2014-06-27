0a8fb390c54776726e8c67107f6d20a3c423dee0
cartersteven <stevencarter@fullsail.edu>
2014-06-26 23:00:09 -0400

    Functions Industry
    This commit adds the beginning of the work for this project.

--- Week 4/Functions_Graded/Functions_Industry/js/script.js
+++ Week 4/Functions_Graded/Functions_Industry/js/script.js
@@ -1,5 +1,13 @@
-//This program will .
+//This program will calculate the actual download speed you can get through your Internet Service Provider.
 /**
  * Created by stevencarter on 6/21/14.
  */
+
+var companySpeedMultiplier = function (comp,adspeed){
+    var companies["Bright House", "Comcast", "Verizon", "AT&T", "Hughes Net", "Juno", "Time Warner", "Google"];
+    var percentage[.15, .08, .1, .1, .8, 1, .08, .5];
+    var pos = companies.indexOf(comp); //This finds the position of the speed ratio number in the companies array.
+    var multiplier = percentage[pos];
+    var actspd = adspeed * multiplier
+};
 
